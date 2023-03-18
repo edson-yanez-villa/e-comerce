1820ca9323c8c90c82378bba234f0712a6493194
Moises Edson Yañez Villa <yanezvilla.edson@gmail.com>
2023-03-17 21:14:13 -0400

    Initial project

---
+++ app/src/layouts/MainLayout.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title> Quasar App </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-footer bordered class="bg-white text-primary">
+      <q-tabs
+        no-caps
+        active-color="primary"
+        indicator-color="transparent"
+        class="text-grey"
+      >
+        <q-tab name="images">
+          <q-btn dense color="blue" round icon="shopping_cart" class="q-ml-md">
+            <q-badge color="red" floating>4</q-badge>
+          </q-btn>
+        </q-tab>
+        <q-tab name="videos" label="Videos" />
+        <q-tab name="articles" label="Articles" />
+      </q-tabs>
+    </q-footer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup></script>
---
+++ app/src/pages/IndexPage.vue
@@ -0,0 +1,40 @@
+<script setup></script>
+
+<template>
+  <q-page class="flex flex-center">
+    <q-list bordered padding>
+      <q-item-label header>List Header</q-item-label>
+
+      <q-item>
+        <q-item-section top avatar>
+          <q-avatar
+            color="primary"
+            text-color="white"
+            square
+            icon="bluetooth"
+          />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>Single line item</q-item-label>
+          <q-item-label caption
+            >Secondary line text. Lorem ipsum dolor sit amet, consectetur
+            adipiscit elit.</q-item-label
+          >
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-btn
+            dense
+            color="blue"
+            round
+            icon="add_shopping_cart"
+            class="q-ml-md"
+          >
+          </q-btn>
+          <q-item-label caption>Add Cart</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-page>
+</template>
